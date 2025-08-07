98e075ddba4fb6757393c6ba9d5c572e62ed0655
takayukitsujimoto <ttsujimotomed@gmail.com>
2025-08-07 23:26:54 +0900

    Fix: valid Firebase API key and auth setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBu-HirQvEdFh72rSNp5tlbDanY3_TCaUc",
   authDomain: "pathologyquizapp.firebaseapp.com",
@@ -6,3 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "281543874376",
   appId: "1:281543874376:web:2302f0654937a3a7e9457d"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
