10f7c42e8098d0a8b9f87bf7659789390c645792
takayukitsujimoto <ttsujimotomed@gmail.com>
2025-08-07 11:22:05 +0900

    Add all-category random quiz mode

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "あなたのAPIキー",
-  authDomain: "xxxx.firebaseapp.com",
-  projectId: "あなたのプロジェクトID",
-  storageBucket: "xxxx.appspot.com",
-  messagingSenderId: "xxxxxxxx",
-  appId: "xxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyBu-HirQvEdFh72rSNp5tlbDanY3_TCaUc",
+  authDomain: "pathologyquizapp.firebaseapp.com",
+  projectId: "pathologyquizapp",
+  storageBucket: "pathologyquizapp.firebasestorage.app",
+  messagingSenderId: "281543874376",
+  appId: "1:281543874376:web:2302f0654937a3a7e9457d"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
